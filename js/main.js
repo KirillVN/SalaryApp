3119c6b7cfb5dd4a523c7edf342632d2a8f5ace2
KirillVN <kirill137777@gmail.com>
2022-04-09 23:09:25 +0300

    version2.0

--- js/main.js
+++ js/main.js
@@ -1,34 +1,52 @@
-const inputName = document.querySelector('.input-name');
-const inputSalary = document.querySelector('.input-salary');
-const btnSbmt = document.querySelector('.btn-submit');
-const content = document.querySelector('.content-container');
-let data = [];
-
-
-btnSbmt.addEventListener('click', getInput);
-
-
+const inputName = document.querySelector(".input-name");
+const inputSalary = document.querySelector(".input-salary");
+const btnSbmt = document.querySelector(".btn-submit");
+const content = document.querySelector(".content-container");
+let data;
+!localStorage.info
+  ? (data = [])
+  : (data = JSON.parse(localStorage.getItem("info")));
+btnSbmt.addEventListener("click", getInput);
+fillHtml();
 function updateLocal() {
-    localStorage.setItem('info', JSON.stringify(data))
+  localStorage.setItem("info", JSON.stringify(data));
+}
+function createObj() {
+  if (inputName.value && inputSalary.value) {
+    let obj = {
+      name: inputName.value,
+      price: inputSalary.value,
+    };
+    data.push(obj);
+  }
+}
+
+function createElement(obj, index) {
+  return `<div class="text-name">${obj.name}</div>
+        <div class="text-slr">${obj.price}&#8381</div>
+        `;
 }
 function getInput(event) {
-    event.preventDefault();
-    if(inputName.value && inputSalary.value) {
-        createObj()
-        content.innerHTML += `
-        <div class="text-name">${inputName.value}</div>
-        <div class="text-slr">${inputSalary.value}&#8381</div>
-        `
-        updateLocal()
-        inputName.value = ''
-        inputSalary.value = ''
-    }
+  event.preventDefault();
+  createObj();
+  fillHtml();
+  updateLocal();
+  inputName.value = "";
+  inputSalary.value = "";
 }
 
-function createObj() {
-    let obj = {
-        name: inputName.value,
-        price: inputSalary.value,
-    }
-    data.push(obj)
+function fillHtml() {
+  content.innerHTML = "";
+  if (data.length > 0) {
+    data.forEach((role, index) => {
+      content.innerHTML += createElement(role, index);
+    });
+  }
 }
+
+//   if (inputName.value && inputSalary.value) {
+//     // content.innerHTML += `
+//     //     <div class="text-name">${inputName.value}</div>
+//     //     <div class="text-slr">${inputSalary.value}&#8381</div>
+//     //     `;
+//   }
