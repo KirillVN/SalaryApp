1314e73e6a4c9a38fbdbc7ba503758cd393903e7
KirillVN <kirill137777@gmail.com>
2022-04-10 09:53:37 +0300

    vers2.1

--- js/main.js
+++ js/main.js
@@ -1,16 +1,35 @@
+
 const inputName = document.querySelector(".input-name");
 const inputSalary = document.querySelector(".input-salary");
 const btnSbmt = document.querySelector(".btn-submit");
 const content = document.querySelector(".content-container");
+const btnsTrash = document.querySelector(".trash");
+
+btnsTrash.addEventListener('click', () => {
+  
+}) 
+
+
+
+//SET AN ARRAY
 let data;
+// CHECK IF A LOCAL STORAGE IS NOT EMPTHY
 !localStorage.info
   ? (data = [])
   : (data = JSON.parse(localStorage.getItem("info")));
+
 btnSbmt.addEventListener("click", getInput);
-fillHtml();
-function updateLocal() {
-  localStorage.setItem("info", JSON.stringify(data));
+
+// GET DATA FROM INPUT
+function getInput(event) {
+  event.preventDefault();
+  createObj();
+  fillHtml();
+  updateLocal();
+  inputName.value = "";
+  inputSalary.value = "";
 }
+// CREATE and ADD INTO ARR
 function createObj() {
   if (inputName.value && inputSalary.value) {
     let obj = {
@@ -21,20 +40,22 @@ function createObj() {
   }
 }
 
+function updateLocal() {
+  localStorage.setItem("info", JSON.stringify(data));
+}
+//  CREATE TEMPLATE
 function createElement(obj, index) {
-  return `<div class="text-name">${obj.name}</div>
-        <div class="text-slr">${obj.price}&#8381</div>
+  return `
+  <div class="content-container">
+  <div class="text-name">${obj.name}</div>
+  <div class="text-slr">${obj.price}&#8381</div>
+  <button class="trash">
+   <i class="fa-solid fa-trash-can"></i>
+  </button>
+</div>
         `;
 }
-function getInput(event) {
-  event.preventDefault();
-  createObj();
-  fillHtml();
-  updateLocal();
-  inputName.value = "";
-  inputSalary.value = "";
-}
-
+// FILL PAGE FROM ARR
 function fillHtml() {
   content.innerHTML = "";
   if (data.length > 0) {
@@ -43,10 +64,5 @@ function fillHtml() {
     });
   }
 }
+fillHtml();
 
-//   if (inputName.value && inputSalary.value) {
-//     // content.innerHTML += `
-//     //     <div class="text-name">${inputName.value}</div>
-//     //     <div class="text-slr">${inputSalary.value}&#8381</div>
-//     //     `;
-//   }
