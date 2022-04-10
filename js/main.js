235fa773818a11865a1732a410906bce2a3f0740
KirillVN <kirill137777@gmail.com>
2022-04-10 10:07:46 +0300

    some commit

--- js/main.js
+++ js/main.js
@@ -1,34 +1,65 @@
-const inputName = document.querySelector('.input-name');
-const inputSalary = document.querySelector('.input-salary');
-const btnSbmt = document.querySelector('.btn-submit');
-const content = document.querySelector('.content-container');
-let data = [];
+
+const inputName = document.querySelector(".input-name");
+const inputSalary = document.querySelector(".input-salary");
+const btnSbmt = document.querySelector(".btn-submit");
+const content = document.querySelector(".content-container");
 
 
-btnSbmt.addEventListener('click', getInput);
 
+
+
+//SET AN ARRAY
+let data;
+// CHECK IF A LOCAL STORAGE IS NOT EMPTHY
+!localStorage.info
+  ? (data = [])
+  : (data = JSON.parse(localStorage.getItem("info")));
+
+btnSbmt.addEventListener("click", getInput);
+
+// GET DATA FROM INPUT
+function getInput(event) {
+  event.preventDefault();
+  createObj();
+  fillHtml();
+  updateLocal();
+  inputName.value = "";
+  inputSalary.value = "";
+}
+// CREATE and ADD INTO ARR
+function createObj() {
+  if (inputName.value && inputSalary.value) {
+    let obj = {
+      name: inputName.value,
+      price: inputSalary.value,
+    };
+    data.push(obj);
+  }
+}
 
 function updateLocal() {
-    localStorage.setItem('info', JSON.stringify(data))
+  localStorage.setItem("info", JSON.stringify(data));
 }
-function getInput(event) {
-    event.preventDefault();
-    if(inputName.value && inputSalary.value) {
-        createObj()
-        content.innerHTML += `
-        <div class="text-name">${inputName.value}</div>
-        <div class="text-slr">${inputSalary.value}&#8381</div>
-        `
-        updateLocal()
-        inputName.value = ''
-        inputSalary.value = ''
-    }
+//  CREATE TEMPLATE
+function createElement(obj, index) {
+  return `
+  <div class="content-container">
+  <div class="text-name">${obj.name}</div>
+  <div class="text-slr">${obj.price}&#8381</div>
+  <button class="trash">
+   <i class="fa-solid fa-trash-can"></i>
+  </button>
+</div>
+        `;
 }
+// FILL PAGE FROM ARR
+function fillHtml() {
+  content.innerHTML = "";
+  if (data.length > 0) {
+    data.forEach((role, index) => {
+      content.innerHTML += createElement(role, index);
+    });
+  }
+}
+fillHtml();
 
-function createObj() {
-    let obj = {
-        name: inputName.value,
-        price: inputSalary.value,
-    }
-    data.push(obj)
-}
